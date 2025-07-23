e30a887f5eff64aaf0ca8aeec3016beeedb9b99f
Alex Khizhnyi <mendler88@gmail.com>
2025-07-23 17:21:40 +0300

    fix export type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export {type IRuleFn, type IExtendProp, type IExtend, type IFelaRenderer, type IExtendRule} from './fela.interface';
+export {
+    type IRuleFn, type IExtendProp, type IExtend, type IFelaRenderer, type IExtendRule, type ICachedStyle
+} from './fela.interface';
 export {createRules} from './createRules';
 export {useStyle} from './_hooks/useStyle';
 export {FelaRendererContext} from './renderer.context';
